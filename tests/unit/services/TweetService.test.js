e06847578724c173d301e4a19178cb59cebefca4
Wesam Mustafa <wesam.mustafa100@gmail.com>
2019-07-30 08:52:31 +0200

    Finish unit test

--- tests/unit/services/TweetService.test.js
+++ tests/unit/services/TweetService.test.js
@@ -23,13 +23,46 @@ describe('TweetService', function () {
       TweetService.fetch(params);
       expect(clientGetStub.firstCall.args[1]).to.equal(params);
     });
+
+    it('should return `promise object` contains array of tweets', function () {
+      clientGetStub.returns(Promise.resolve([{ tweetId: 1 }]));
+      const params = { user_id: 1 };
+      TweetService.fetch(params);
+      const { returnValue } = clientGetStub.firstCall;
+      return expect(returnValue).to.eventually.deep.equal([{ tweetId: 1 }]);
+    });
   });
 
-  it('should `format` tweets from user time line ', function () {
-
-  });
-  it('should `delete` old tweets from db', function () {
-
+  it('should take an `array of tweets` from user timeline and return `formatted array of tweets` ', function () {
+    const tweets = [
+      {
+        text: 'hello! this is the first tweet from fictionfone.',
+        retweetCount: 0,
+        _id: '5d3fe0dfe2f39a34d360dda8',
+        id_str: '937260813135634432',
+        created_at: 'Sun Dec 03 10:02:45 +0000 2017',
+        entities: {
+          hashtags: [],
+          symbols: [],
+          user_mentions: [],
+          urls: [],
+        },
+        user: {
+          id: 935805382236176400,
+          name: 'FictionFone',
+          screenName: 'FictionFone',
+          profileImageUrlHttps: 'https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png',
+          profile_image_url_https: 'https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png',
+          screen_name: 'FictionFone',
+        },
+        createdAt: '2017-12-03T10:02:45.000Z',
+        __v: 0,
+        updatedAt: '2019-07-30T06:17:03.323Z',
+        retweet_count: 0,
+      }];
+    
+    const result = TweetService.format(tweets);
+    expect(result[0].tweetId).to.equal(tweets[0].id_str);
   });
 
   describe('save', function () {
